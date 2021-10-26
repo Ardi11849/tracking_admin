ff1d1cf0b761fbf2609b485b0ef7be065a7b95a4
Ardi11849 <ardisuryana115@gmail.com>
2021-10-26 16:51:12 +0700

    Update Alur

--- application/models/Tracking_m.php
+++ application/models/Tracking_m.php
@@ -1,25 +1,2 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-class Tracking_m extends CI_Model {
-	
-	var $api_url = 'https://damp-shore-65068.herokuapp.com/';
-
-	public function GetAll(){
-		$result = $this->curl->simple_get($this->api_url.'api/v1/getPosition');
-		if (!$result) return json_encode(array('message' => $this->curl->error_string), true);
-		return $result;
-	}
-
-	public function GetByIdPerusahaan(){
-		$result = $this->curl->simple_get($this->api_url.'api/v1/getPositionByPerusahaan?IdPerusahaan='.$this->session->userdata("IdPerusahaan"));
-		if (!$result) return json_encode(array('message' => $this->curl->error_string), true);
-		return $result;
-	}
-
-	public function GetByIdCabang(){
-		$result = $this->curl->simple_get($this->api_url.'api/v1/getPositionByCabang?IdCabang='.$this->session->userdata("IdCabang"));
-		if (!$result) return json_encode(array('message' => $this->curl->error_string), true);
-		return $result;
-	}
-
-}
-
+class Tracking_m extends CI_Model {var $api_url = 'https://damp-shore-65068.herokuapp.com/';public function GetAll(){$result = $this->curl->simple_get($this->api_url.'api/v1/getPosition');if (!$result) return json_encode(array('message' => $this->curl->error_string), true);return $result;}public function GetByIdPerusahaan(){$result = $this->curl->simple_get($this->api_url.'api/v1/getPositionByPerusahaan?IdPerusahaan='.$this->session->userdata("IdPerusahaan"));if (!$result) return json_encode(array('message' => $this->curl->error_string), true);return $result;}public function GetByIdCabang(){$result = $this->curl->simple_get($this->api_url.'api/v1/getPositionByCabang?IdCabang='.$this->session->userdata("IdCabang"));if (!$result) return json_encode(array('message' => $this->curl->error_string), true);return $result;}}
